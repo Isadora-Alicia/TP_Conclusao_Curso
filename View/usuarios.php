8181a279aee3e8ce86bc16bb0eaa2837143e22b7
Victor Marcolino <vctor.marcolino@gmail.com>
2024-07-03 21:58:17 -0300

    aula 03/07

---
+++ View/usuarios.php
@@ -0,0 +1,25 @@
+<html>
+    <head>Gestão de Usuario</head>
+    <body>
+      <form action="" method="post">
+        <label for="nome">Nome: </label>
+        <input type="text" id="nome" name="nome">
+
+        <label for="email">Email: </label>
+        <input type="email" id="email" name="email">
+
+        <label for="senha">Senha: </label>
+        <input type="passaword" id="senha" name="senha">
+
+        <select id="papel" name="papel">
+
+        <option value="coordenador">Coordenador</option>
+        <option value="professor">Professor</option>
+        </select>
+
+        <button type="submit">Adicionar Usuário</button>
+
+
+      </form>  
+    </body>
+</html>
